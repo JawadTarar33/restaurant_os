a55b51819d907fbe081c6a6413a3fc1db3c891fb
Jawad Tarar <110728502+JawadTarar33@users.noreply.github.com>
2025-12-19 15:10:55 +0500

    Refactor media settings in Django configuration
    Updated MEDIA_URL and MEDIA_ROOT to use Pathlib. Added DEFAULT_FILE_STORAGE setting.

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -128,8 +128,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 import os
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+
+
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
